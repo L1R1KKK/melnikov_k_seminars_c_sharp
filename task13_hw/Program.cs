8621262a54ce7cb1ab6b7db3cc2f3bda2c88faf9
L1R1KKK <work.melnikov@yandex.ru>
2023-09-24 09:36:25 +0300

    task13_hw

--- task13_hw/Program.cs
+++ task13_hw/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №10
+// 1.Напишите программу, которая выводит третью цифру заданного числа 
+// 2.или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int ThirdDigit(int num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
+bool LessDigit(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("Ошибка, число двухзначное!");
+        return false;
+    }
+    return true;
+}
+int number;
+Console.WriteLine("Введите трехзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+if (LessDigit(number))
+{
+    Console.WriteLine(ThirdDigit(number));
+}
