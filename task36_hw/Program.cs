f80d108cd220127de335870cf86ece022097e001
L1R1KKK <work.melnikov@yandex.ru>
2023-10-07 22:35:01 +0300

    task36

--- task36_hw/Program.cs
+++ task36_hw/Program.cs
@@ -4,21 +4,21 @@
 // [-4, -6, 89, 6] -> 0
 
 Console.Write($"Введи количество элементов массива: ");
-int numberElements = Convert.ToInt32(Console.ReadLine()); //просим кол-во эллементов для RandomNubers
+int numberElements = Convert.ToInt32(Console.ReadLine()); //просим кол-во эллементов массива для RandomNubers
 
 int RandomNumbers(int numberElements, int min, int max)
 {
     int[] randomNumbers = new int[numberElements];
     int sumElements = 0;
-    Console.Write("Получившийся массив: ");
-
+    Console.Write("Получившийся массив: "); //сделал специально что бы воводилась надпись перед массивом
+    Random rnd = new Random();
     for (int i = 0; i < randomNumbers.Length; i++)
     {
-        randomNumbers[i] = new Random().Next(min, max);
+        randomNumbers[i] = rnd.Next(min, max);
 
         Console.Write(randomNumbers[i] + " ");
 
-        if (i % 2 != 1)
+        if (i % 2 != 1) // проверка четной позиции
         {
             sumElements = sumElements + randomNumbers[i];
         }
@@ -26,6 +26,6 @@ int RandomNumbers(int numberElements, int min, int max)
     return sumElements;
 }
 
-int randomNumbers = RandomNumbers(numberElements, 1, 10);
+int randomNumbers = RandomNumbers(numberElements, 1, 10); //диапозон
 Console.WriteLine();
 Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {randomNumbers}");
