03da711e949778356c9a3becaf38c5ad695046fe
L1R1KKK <work.melnikov@yandex.ru>
2023-10-07 16:35:07 +0300

    task25, task27, task29 HW

---
+++ task24/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum += i;  //sum = sum + i
+        }
+        
+    }
+    return sum;
+}
+
+Console.WriteLine("Input digital");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Error!");
+    return;
+}
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Sum digital from 1 to A {number} = {sumNumbers}");
+
+
+
+
+// int j = 2048;
+// byte bb = (byte)j;
+// Console.WriteLine(bb);
---
+++ task29_hw/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string message) //Ввод числа с экрана
+{
+    Console.Write(message); //Сам ввод
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput); // Перевод string в int
+    return result; // Получение результата
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue) // Метод для получения случайных значений массива
+{
+    int[] array = new int[Length]; // Задаем массив
+    Random random = new Random();  // Генерация цифр массива
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1); // Заполняем случайными цифрами из даиапозона min -> max
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // Наш массив
+{
+    Console.Write("["); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine("]");
+}
+
+int length = Prompt("Array length ");
+int min = Prompt("Start array - ");
+int max = Prompt("End array - ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
