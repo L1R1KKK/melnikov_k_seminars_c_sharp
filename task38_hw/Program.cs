9598a24ef54630202350ee51480eb97f07d09470
L1R1KKK <work.melnikov@yandex.ru>
2023-10-08 00:14:50 +0300

    task38

--- task38_hw/Program.cs
+++ task38_hw/Program.cs
@@ -1,27 +1,35 @@
-﻿
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+Console.Clear();
+double[] arrayRealNumbers = new double[10]; //создаем наш массив с кол-во элментов
 
-double[] arrayRealNumbers = new double[10];
-  for (int i = 0; i < arrayRealNumbers.Length; i++ )
-  {
-    arrayRealNumbers[i] = new Random().Next(1, 10);
-    Console.Write(arrayRealNumbers[i] + " ");
-  }
+Random rnd = new Random();
+double min = 10;
+double max = 100;
+Console.Write($"Получившийся массив -> ");
+for (int i = 0; i < arrayRealNumbers.Length; i++) // даем диапозон и выводим на экран
+{
+    arrayRealNumbers[i] = rnd.NextDouble() * (max - min) + min ;
+    
+    Console.Write($"{arrayRealNumbers[i]:F2}  ");
+}
 
 double maxNumber = arrayRealNumbers[0];
 double minNumber = arrayRealNumbers[0];
 
-  for (int i = 1; i < arrayRealNumbers.Length; i++)
-  {
+for (int i = 1; i < arrayRealNumbers.Length; i++)
+{
     if (maxNumber < arrayRealNumbers[i])
     {
-      maxNumber = arrayRealNumbers[i];
+        maxNumber = arrayRealNumbers[i];
     }
-        if (minNumber > arrayRealNumbers[i])
+    if (minNumber > arrayRealNumbers[i])
     {
-      minNumber = arrayRealNumbers[i];
+        minNumber = arrayRealNumbers[i];
     }
-  }
+}
 
-  double decision = maxNumber - minNumber;
-
-  Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {decision}");
+double decision = maxNumber - minNumber;
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным ({maxNumber:F2}) и минимальным({minNumber:F2}) элементами: {decision:F2}");
